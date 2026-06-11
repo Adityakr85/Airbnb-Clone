2314daef40cda7261e794104235f1285c26e9e72
Jyoti0905 <jyotikumari090504@gmail.com>
2026-06-11 19:56:06 +0530

    Change 'Become a host' link URL

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -137,11 +137,8 @@ export default function Navbar() {
         )}
 
         <div className="z-20 flex items-center gap-4">
-          <Link
-            to="/host"
-            className="hidden rounded-full px-4 py-3 font-semibold transition hover:bg-gray-100 md:block"
-          >
-            Become a host
+          <Link to="/become-a-host" className="hidden rounded-full px-4 py-3 font-semibold transition hover:bg-gray-100 md:block">
+                    Become a host
           </Link>
 
           <button className="flex h-11 w-11 items-center justify-center rounded-full bg-gray-100 transition hover:bg-gray-200">
